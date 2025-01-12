f4e9f406c96be359fb71e37815ab13db8d71740e
TasnimSourour <sorour.tasnim97@gmail.com>
2025-01-12 17:53:47 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// alert('welcome')
+let icon = document.getElementById('icon-mobile')
+let ul = document.getElementById('ul-mobile')
+
+icon.onclick = function () {
+    ul.classList.toggle("mobile-nav1");
+}
